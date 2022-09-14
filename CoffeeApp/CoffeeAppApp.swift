d9cb4b8ee18ba06396ff2f4e64cb2460a653a3f7
Shahadbagarish <Shahad.bagarish@gmail.com>
2022-09-14 10:10:21 +0300

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Shahad Bagarish on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
